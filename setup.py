8969b00576f804681b809c91e61d129b53375165
stinkyfart69420 <147311474+stinkyfart69420@users.noreply.github.com>
2024-02-20 12:05:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-# config.py
 import subprocess
 
 subprocess.run(["pip", "install", "discord.py", "accuweather", "urbandictionary_top", "requests", "--break-system-packages"])
 
 TOKEN = input("Enter your Discord bot token: ")
 
-# Read the main.py file content
 with open("main.py", "r") as file:
     main_content = file.read()
 
-# Replace "BOT_TOKEN" with the actual token
 main_content = main_content.replace("BOT_TOKEN", TOKEN)
 
-# Write the modified content back to main.py
 with open("main.py", "w") as file:
     file.write(main_content)
 
